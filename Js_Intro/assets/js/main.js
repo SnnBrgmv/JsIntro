81880f0cae0e789b5780759f82955c7410ce71a3
SnnBrgmv <sananai@code.edu.az>
2022-10-12 00:53:09 +0400

    JS_Intro

---
+++ Js_Intro/assets/js/main.js
@@ -0,0 +1,11 @@
+var uName = [];
+do {
+    var username = prompt(`Istifadeci adini daxil edin`);
+    var toLowerName = username.toLowerCase();
+    if (uName.includes(toLowerName)) {
+        alert(`Bu istifadeci artiq movcuddur`);
+    } else {
+        uName.push(toLowerName);
+    }
+    console.log(uName);
+} while (username != null);
